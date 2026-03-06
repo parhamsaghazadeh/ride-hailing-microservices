8bb6d25eef4b9383d930a8f46465854fd5bcc1e1
Parham <parhamsaghazadeh@gmail.com>
2026-03-06 17:54:38 +0330

    config user

---
+++ services/user-service/src/main/java/org/example/factory/SBExampleApplication.java
@@ -0,0 +1,16 @@
+package org.example.factory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class SBExampleApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(SBExampleApplication.class, args);
+    }
+
+
+}
