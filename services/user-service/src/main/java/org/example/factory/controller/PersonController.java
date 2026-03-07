266d8472eef7caed32cad5a16f5daaa1e203f127
Parham <parhamsaghazadeh@gmail.com>
2026-03-07 19:15:57 +0330

    add PersonController

---
+++ services/user-service/src/main/java/org/example/factory/controller/PersonController.java
@@ -0,0 +1,90 @@
+package org.example.factory.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.factory.Model.Converter;
+import org.example.factory.Model.PersonModel;
+import org.example.factory.entity.Person;
+import org.example.factory.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequestMapping("/person")
+@RestController
+public class PersonController {
+    @Autowired
+    private PersonService personService;
+    @Autowired
+    private Converter converter;
+
+    @GetMapping
+    public ResponseEntity<List<PersonModel>> retrievePerson() {
+        try {
+            List<Person> person = personService.getPersons();
+            List<PersonModel> personModels = person.stream()
+                    .map(converter::PersonConvert)
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok(personModels);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<PersonModel>> searchPerson(@RequestParam long id) {
+        try {
+            Person person = personService.getPerson(id);
+            PersonModel personModel = converter.PersonConvert(person);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<PersonModel> createPerson(@RequestBody Person person) {
+        try {
+            Person person1 = personService.createPerson(person);
+            PersonModel personModel = converter.PersonConvert(person1);
+            return ResponseEntity.ok(personModel);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<PersonModel> updatePerson(@RequestBody Person person) {
+        try {
+            Person person1 = personService.updatePerson(person);
+            PersonModel personModel = converter.PersonConvert(person1);
+            return ResponseEntity.ok(personModel);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deletePerson(@RequestParam long id) {
+        try {
+            personService.deletePerson(id);
+            return ResponseEntity.ok().build();
+        }catch (Exception e) {
+            log.error(e.getMessage());
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
