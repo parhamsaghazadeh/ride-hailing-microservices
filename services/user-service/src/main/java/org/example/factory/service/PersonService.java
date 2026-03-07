c27321696d1f80bf3286cfa5e124abcdb76d38d1
Parham <parhamsaghazadeh@gmail.com>
2026-03-07 18:26:13 +0330

    add PersonService

---
+++ services/user-service/src/main/java/org/example/factory/service/PersonService.java
@@ -0,0 +1,37 @@
+package org.example.factory.service;
+
+import org.example.factory.entity.Person;
+import org.example.factory.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonService {
+    private PersonRepository personRepository;
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public Person createPerson(Person person) {
+        return personRepository.save(person);
+    }
+
+    public Person updatePerson(Person person) {
+        return personRepository.save(person);
+    }
+
+    public List<Person> getPersons(){
+        return personRepository.findAll();
+    }
+
+    public void deletePerson(Person person) {
+        personRepository.delete(person);
+    }
+
+    public Person getPerson(Long id) {
+        return personRepository.findById(id).orElse(null);
+    }
+}
