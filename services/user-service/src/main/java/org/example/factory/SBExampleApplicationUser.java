85c62051440aa8006a7c999c49b0c784adac5a25
Parham <parhamsaghazadeh@gmail.com>
2026-03-07 16:34:28 +0330

    fixe code

--- services/user-service/src/main/java/org/example/factory/SBExampleApplicationUser.java
+++ services/user-service/src/main/java/org/example/factory/SBExampleApplicationUser.java
@@ -2,14 +2,18 @@ package org.example.factory;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
+@SpringBootApplication(scanBasePackages = {
+"org.example.factory"
+})
 @Configuration
-public class SBExampleApplication {
+public class SBExampleApplicationUser {
     public static void main(String[] args) {
 
-        SpringApplication.run(SBExampleApplication.class, args);
+        SpringApplication.run(SBExampleApplicationUser.class, args);
     }
 
 
