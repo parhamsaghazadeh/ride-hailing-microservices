89bde8c5cb881dbe0bf80026a7b47746b5b16fd5
Parham <parhamsaghazadeh@gmail.com>
2026-03-09 19:21:18 +0330

    add SBExampleApplicationUser

---
+++ services/ride-service/src/main/java/org/example/factory/SBExampleApplicationUser.java
@@ -0,0 +1,16 @@
+package org.example.factory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@SpringBootApplication(scanBasePackages = "org.example.factory")
+@Configuration
+public class SBExampleApplicationUser {
+    public static void main(String[] args) {
+        SpringApplication.run(SBExampleApplicationUser.class, args);
+    }
+}
