48022332d7bdb5fa4a785148f0e35e47846d3bac
Parham <parhamsaghazadeh@gmail.com>
2026-03-14 16:10:46 +0330

    add PersonWallet

---
+++ services/user-service/src/main/java/org/example/factory/entity/PersonWallet.java
@@ -0,0 +1,31 @@
+package org.example.factory.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "person_wallet")
+@Data
+public class PersonWallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "person_id" , foreignKey = @ForeignKey(name = "FK-person-wallet"))
+    private Person person;
+    //موجودی کیف پول
+    @Column(name = "balance" ,nullable = false , precision = 10 , scale = 2)
+    private BigDecimal balance;
+    //واحد پول
+    @Column(name = "currency" , nullable = false , length = 50)
+    private String currency;
+    //ایجاد زمان پول
+    @Column(name = "created_at" , nullable = false)
+    private LocalDateTime createdAt;
+    //آخرین بروزرسانی کیف پول
+    @Column(name = "update_at" , nullable = false)
+    private LocalDateTime updateAt;
+}
