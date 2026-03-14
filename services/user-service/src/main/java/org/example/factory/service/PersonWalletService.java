4acafeed9e1ffd00d2309b7c3e08ddc1aa3c02d0
Parham <parhamsaghazadeh@gmail.com>
2026-03-14 17:02:17 +0330

    add PersonWalletService

---
+++ services/user-service/src/main/java/org/example/factory/service/PersonWalletService.java
@@ -0,0 +1,38 @@
+package org.example.factory.service;
+
+import org.example.factory.repository.PersonWalletRepository;
+import org.example.factory.entity.PersonWallet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonWalletService {
+    private PersonWalletRepository personWalletRepository;
+
+    @Autowired
+    public PersonWalletService(PersonWalletRepository personWalletRepository) {
+        this.personWalletRepository = personWalletRepository;
+    }
+
+    public PersonWallet getWalletById(Long id){
+        return personWalletRepository.findById(id).orElse(null);
+    }
+
+    public List<PersonWallet> getAllWallets(){
+        return personWalletRepository.findAll();
+    }
+
+    public PersonWallet saveWallet(PersonWallet personWallet) {
+        return personWalletRepository.save(personWallet);
+    }
+
+    public void deleteWallet(long id) {
+        personWalletRepository.deleteById(id);
+    }
+
+    public PersonWallet updateWallet(PersonWallet personWallet) {
+        return personWalletRepository.save(personWallet);
+    }
+}
