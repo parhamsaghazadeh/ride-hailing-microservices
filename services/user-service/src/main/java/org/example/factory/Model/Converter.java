356988e8c4900ddec3c4af835d82d6d2c38ac540
Parham <parhamsaghazadeh@gmail.com>
2026-03-14 17:19:08 +0330

    convert PersonLocation and PersonWallet

--- services/user-service/src/main/java/org/example/factory/Model/Converter.java
+++ services/user-service/src/main/java/org/example/factory/Model/Converter.java
@@ -1,14 +1,18 @@
 package org.example.factory.Model;
 
 import org.example.factory.entity.Person;
+import org.example.factory.entity.PersonLocation;
+import org.example.factory.entity.PersonWallet;
 import org.springframework.stereotype.Component;
 
+import java.text.DecimalFormat;
 import java.time.format.DateTimeFormatter;
 
 @Component
 public class Converter {
 
     public DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public static DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#,##0.00");
     public PersonModel PersonConvert(Person person) {
         PersonModel personModel = new PersonModel();
         personModel.setId(person.getId());
@@ -25,5 +29,23 @@ public class Converter {
             personModel.setAddress(addressModel);
         }
         return personModel;
+    }
+
+    public PersonWalletModel toPersonWalletModel(PersonWallet personWallet) {
+        PersonWalletModel personWalletModel = new PersonWalletModel();
+        personWalletModel.setId(personWallet.getId());
+        personWalletModel.setBalance(DECIMAL_FORMAT.format(personWallet.getBalance()));
+        personWalletModel.setCurrency(personWallet.getCurrency());
+        personWalletModel.setCreated_at(dateTimeFormatter.format(personWallet.getCreatedAt()));
+        personWalletModel.setUpdated_at(dateTimeFormatter.format(personWallet.getCreatedAt()));
+        return personWalletModel;
+    }
+    public PersonLocationModel toPersonLocationModel(PersonLocation personLocation) {
+        PersonLocationModel personLocationModel = new PersonLocationModel();
+        personLocationModel.setId(personLocation.getId());
+        personLocationModel.setNameLocation(personLocation.getLocationName());
+        personLocationModel.setCreatedAt(dateTimeFormatter.format(personLocation.getCreatedAt()));
+        personLocationModel.setIsDefault(personLocationModel.getIsDefault());
+        return personLocationModel;
     }
 }
