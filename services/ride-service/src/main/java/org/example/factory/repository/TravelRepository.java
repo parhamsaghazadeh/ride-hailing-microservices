ce96126a40f41346f993178f9920680071d8bfa1
Parham <parhamsaghazadeh@gmail.com>
2026-03-19 14:04:21 +0330

    delete TravelRepository.java

--- services/ride-service/src/main/java/org/example/factory/repository/TravelRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.factory.repository;
-
-import org.example.factory.entity.Travel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TravelRepository extends JpaRepository<Travel, Long> {
-}
