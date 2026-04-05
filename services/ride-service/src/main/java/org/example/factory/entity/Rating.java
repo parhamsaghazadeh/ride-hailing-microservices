df88e8293aba6877273b7528376f38bae6896c74
Parham <parhamsaghazadeh@gmail.com>
2026-04-05 15:04:25 +0330

    add Rating

---
+++ services/ride-service/src/main/java/org/example/factory/entity/Rating.java
@@ -0,0 +1,33 @@
+package org.example.factory.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.example.factory.model.Enum;
+
+import java.time.LocalDateTime;
+
+
+
+@Entity
+@Table(name = "rating")
+@Data
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "driver_id",nullable = false)
+    private Long driverId;
+    @Column(name = "user_id" , nullable = false)
+    private Long userId;
+    //امتیاز 1-10
+    @Column(name = "score",nullable = false)
+    private Integer score;
+    @Column(name = "comment" , nullable = false ,length = 250)
+    private String comment;
+    @Column(name = "time_stamp" , nullable = false)
+    private LocalDateTime timeStamp;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rating_level" , nullable = false)
+    private Enum.RatingLevel ratingLevel;
+
+}
