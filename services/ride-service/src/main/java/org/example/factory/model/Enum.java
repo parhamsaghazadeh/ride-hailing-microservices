e9a6fdeae450a0fa6aa0c89463a8911acf4e21a2
Parham <parhamsaghazadeh@gmail.com>
2026-04-05 15:04:08 +0330

    add Enum class

---
+++ services/ride-service/src/main/java/org/example/factory/model/Enum.java
@@ -0,0 +1,11 @@
+package org.example.factory.model;
+
+public class Enum {
+    // rating class
+    public enum RatingLevel{
+        BAD,
+        GOD,
+        VERY_GOOD,
+        EXCELLENT
+    }
+}
