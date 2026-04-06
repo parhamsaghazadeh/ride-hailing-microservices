3774d604e8614c9ccf66dee9426610c9d0adb1a4
Parham <parhamsaghazadeh@gmail.com>
2026-04-06 15:17:54 +0330

    add UserClient to ride service

---
+++ services/ride-service/src/main/java/org/example/factory/client/UserClient.java
@@ -0,0 +1,12 @@
+package org.example.factory.client;
+
+import org.example.factory.model.UserResponseModel;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "user-service")
+public interface UserClient {
+    @GetMapping("/api/users/email/{email}")
+    UserResponseModel getUserByEmail(@PathVariable("email")  long id );
+}
