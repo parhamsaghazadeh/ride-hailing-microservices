277a5c69de91ebc90ea7b8eda49548c5b548a1cb
Parham <parhamsaghazadeh@gmail.com>
2026-04-08 19:14:27 +0330

    add RideService

--- services/ride-service/src/main/java/org/example/factory/service/RideService.java
+++ services/ride-service/src/main/java/org/example/factory/service/RideService.java
@@ -1,7 +1,5 @@
 package org.example.factory.service;
 
-import org.apache.catalina.User;
-import org.example.factory.RestTemplate;
 import org.example.factory.entity.Ride;
 import org.example.factory.repository.RideRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,22 +11,9 @@ import java.util.List;
 public class RideService {
 
     private RideRepository rideRepository;
-
-    private RestTemplate restTemplate;
-
     @Autowired
     public RideService(RideRepository rideRepository) {
         this.rideRepository = rideRepository;
-    }
-
-
-    public Ride savaRide(Ride ride) {
-        return rideRepository.save(ride);
-
-        try {
-            String url = "http://localhost:8080/user/" + ride.getUserId();
-            
-        }
     }
 
     public List<Ride> getAllTravels() {
