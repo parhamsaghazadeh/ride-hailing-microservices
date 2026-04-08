9899afd24405e1461cf96d5f9c421eefe87984f6
Parham <parhamsaghazadeh@gmail.com>
2026-04-08 18:58:55 +0330

    fixe code

--- services/ride-service/src/main/java/org/example/factory/repository/RideRepository.java
+++ services/ride-service/src/main/java/org/example/factory/repository/RideRepository.java
@@ -1,7 +1,10 @@
 package org.example.factory.repository;
 
+import org.example.factory.entity.Rating;
 import org.example.factory.entity.Ride;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RideRepository extends JpaRepository<Ride, Long> {
+@Repository
+public interface RideRepository extends JpaRepository<Rating, Long> {
 }
