ace956f331c5f54907ad91e10fbdc6213b49a900
Parham <parhamsaghazadeh@gmail.com>
2026-04-08 18:58:24 +0330

    fixe code

--- services/user-service/src/main/java/org/example/factory/repository/PersonLocationRepository.java
+++ services/user-service/src/main/java/org/example/factory/repository/PersonLocationRepository.java
@@ -3,7 +3,9 @@ package org.example.factory.repository;
 import org.example.factory.Model.PersonLocationModel;
 import org.example.factory.entity.PersonLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonLocationRepository extends JpaRepository<PersonLocation, Long> {
 
 }
--- services/user-service/src/main/java/org/example/factory/repository/PersonWalletRepository.java
+++ services/user-service/src/main/java/org/example/factory/repository/PersonWalletRepository.java
@@ -2,6 +2,8 @@ package org.example.factory.repository;
 
 import org.example.factory.entity.PersonWallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonWalletRepository extends JpaRepository<PersonWallet, Long> {
 }
