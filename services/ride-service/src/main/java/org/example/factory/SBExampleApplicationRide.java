08947057c1f4b04450cd2c82a9e9ff5749c86000
Parham <parhamsaghazadeh@gmail.com>
2026-04-13 22:58:13 +0330

    add scanner package

--- services/ride-service/src/main/java/org/example/factory/SBExampleApplicationRide.java
+++ services/ride-service/src/main/java/org/example/factory/SBExampleApplicationRide.java
@@ -3,11 +3,16 @@ package org.example.factory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Slf4j
-@SpringBootApplication(scanBasePackages = "org.example.factory")
-@Configuration
+@SpringBootApplication(scanBasePackages = {"org.example.factory"})
+@ComponentScan({"org.example.factory.client" , "org.example.factory.controller" , "org.example.factory.service" , "org.example.factory.repository"})
+@EnableJpaRepositories("org.example.factory.repository")
+@EnableFeignClients("org.example.factory.client")
 public class SBExampleApplicationRide {
     public static void main(String[] args) {
         SpringApplication.run(SBExampleApplicationRide.class, args);
