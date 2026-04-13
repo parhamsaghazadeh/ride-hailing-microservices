56e96faa883a3c795b4fd04c2a118d564f755ffc
Parham <parhamsaghazadeh@gmail.com>
2026-04-13 22:57:44 +0330

    fixe code

--- services/ride-service/src/main/java/org/example/factory/client/UserClient.java
+++ services/ride-service/src/main/java/org/example/factory/client/UserClient.java
@@ -2,11 +2,14 @@ package org.example.factory.client;
 
 import org.example.factory.model.UserResponseModel;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service")
+@Component
+@FeignClient(name = "user-service" , url = "${services.user.url}")
 public interface UserClient {
-    @GetMapping("/api/users/email/{id}")
-    UserResponseModel getUserByEmail(@PathVariable("id")  long id );
+    @GetMapping("/api/users/person/{personId}/role")
+    String getUserRole(@PathVariable("personId") Long driverId);
 }
