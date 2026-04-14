c80509f154defef1ea4b5179ed62a2ccbcd42544
Parham <parhamsaghazadeh@gmail.com>
2026-04-14 15:33:06 +0330

    add findByRole method

--- services/user-service/src/main/java/org/example/factory/repository/PersonRepository.java
+++ services/user-service/src/main/java/org/example/factory/repository/PersonRepository.java
@@ -1,9 +1,14 @@
 package org.example.factory.repository;
 
 import org.example.factory.entity.Person;
+import org.example.factory.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    List<Person> findByRole(Role role);
+
 }
