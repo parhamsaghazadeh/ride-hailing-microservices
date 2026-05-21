16a23ecd6f7c39745d25807c5416d52a0007f14e
Parham <parhamsaghazadeh@gmail.com>
2026-05-21 22:02:51 +0330

    add UserService

---
+++ services/user-service/src/main/java/org/example/factory/service/UserService.java
@@ -0,0 +1,24 @@
+package org.example.factory.service;
+
+import org.example.factory.entity.Person;
+import org.example.factory.repository.PersonRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private PersonRepository personRepository;
+
+    public UserService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public boolean isDriver(Long id) {
+        Person person = personRepository.findById(id).orElse(null);
+
+        if (person == null) {
+            return false;
+        }
+
+        return person.getRole().getTitle().equalsIgnoreCase("Driver");
+    }
+}
