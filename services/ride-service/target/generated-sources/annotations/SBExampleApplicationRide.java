129d87c2a0eae4dca73ba2d688505a1f05da607c
Parham <parhamsaghazadeh@gmail.com>
2026-06-04 15:50:17 +0330

    fixe code

---
+++ services/ride-service/target/generated-sources/annotations/SBExampleApplicationRide.java
@@ -0,0 +1,20 @@
+package org.example.factory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Slf4j
+@SpringBootApplication(scanBasePackages = {"org.example.factory"})
+@ComponentScan({"org.example.factory.client" , "org.example.factory.controller" , "org.example.factory.service" , "org.example.factory.repository"})
+@EnableJpaRepositories("org.example.factory.repository")
+@EnableFeignClients("org.example.factory.client")
+public class SBExampleApplicationUser {
+    public static void main(String[] args) {
+        SpringApplication.run(SBExampleApplicationRide.class, args);
+    }
+}
