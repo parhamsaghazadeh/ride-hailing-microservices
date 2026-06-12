34897d41508feaac61024c5410aed5906922d5a0
Parham <parhamsaghazadeh@gmail.com>
2026-06-12 19:18:50 +0330

    add ResponseEntity

--- services/ride-service/src/main/java/org/example/factory/controller/RideController.java
+++ services/ride-service/src/main/java/org/example/factory/controller/RideController.java
@@ -3,6 +3,8 @@ package org.example.factory.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.example.factory.entity.Ride;
 import org.example.factory.service.RideService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("api/ride")
@@ -17,10 +19,14 @@ public class RideController {
     }
 
     @PostMapping
-    public Ride createRide(
-            @RequestBody Ride ride
-    ) {
-
-        return rideService.createRide(ride);
+    public ResponseEntity<Ride> createRide(@RequestBody Ride ride) {
+        try {
+            rideService.createRide(ride);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ride);
+        }
     }
 }
