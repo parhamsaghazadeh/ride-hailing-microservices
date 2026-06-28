3f4d2c0312d703c8627d0e2da65992145a73352c
Parham <parhamsaghazadeh@gmail.com>
2026-06-28 16:17:39 +0330

    complete RideController

--- services/ride-service/src/main/java/org/example/factory/controller/RideController.java
+++ services/ride-service/src/main/java/org/example/factory/controller/RideController.java
@@ -2,10 +2,15 @@ package org.example.factory.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.factory.entity.Ride;
+import org.example.factory.model.Converter;
+import org.example.factory.model.RideModel;
 import org.example.factory.service.RideService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping("api/ride")
 @RestController
@@ -14,10 +19,14 @@ public class RideController {
 
     private final RideService rideService;
 
-    public RideController(RideService rideService) {
+    private final Converter converter;
+
+    public RideController(RideService rideService, Converter converter) {
+        this.converter = converter;
         this.rideService = rideService;
     }
 
+    //microservice
     @PostMapping
     public ResponseEntity<Ride> createRide(@RequestBody Ride ride) {
         try {
@@ -27,6 +36,70 @@ public class RideController {
             log.error(e.getMessage());
             System.err.println(e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ride);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RideModel>> getRides() {
+        try {
+            List<Ride> rides = rideService.getAllTravels();
+            List<RideModel> rideModels = rides.stream()
+                    .map(converter::convertRideModelToEntity)
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok().body(rideModels);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<RideModel> getId(@RequestParam Long id) {
+        try {
+            Ride ride = rideService.getTravelById(id);
+            RideModel rideModel = converter.convertRideModelToEntity(ride);
+            return ResponseEntity.ok().body(rideModel);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<RideModel> CreateRide(@RequestBody Ride ride) {
+        try {
+            Ride rides = rideService.createRide(ride);
+            return ResponseEntity.ok().body(converter.convertRideModelToEntity(rides));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<RideModel> UpdateRide(@RequestBody Ride ride) {
+        try {
+            Ride rides = rideService.updateTravel(ride);
+            return ResponseEntity.ok().body(converter.convertRideModelToEntity(rides));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> DeleteRide(@RequestParam Long id) {
+        try {
+            rideService.deleteTravel(id);
+            return ResponseEntity.ok().body("ride has been deleted");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
 }
