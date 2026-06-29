fc1791ed8a17fffa4cbfb82e47cead299dde4c0a
Parham <parhamsaghazadeh@gmail.com>
2026-06-29 13:21:50 +0330

    fixe RideController

--- services/ride-service/src/main/java/org/example/factory/controller/RatingController.java
+++ services/ride-service/src/main/java/org/example/factory/controller/RatingController.java
@@ -2,14 +2,16 @@ package org.example.factory.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.factory.entity.Rating;
+import org.example.factory.model.Converter;
+import org.example.factory.model.RatingModel;
 import org.example.factory.service.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/rating")
@@ -18,18 +20,42 @@ public class RatingController {
     @Autowired
     private RatingService ratingService;
 
+    private Converter converter;
+
+    //microservice
     @PostMapping
     public ResponseEntity<Rating> addRating(@RequestBody Rating rating) {
         try {
             ratingService.createRating(rating);
             return ResponseEntity.ok().body(rating);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
 
     }
 
+    @GetMapping
+    public ResponseEntity<List<RatingModel>> getAllRatings() {
+        try {
+            List<Rating> ratings = ratingService.getAllRatings();
+            List<RatingModel> ratingModels = ratings.stream()
+                    .map(converter::convertRatingModelToEntity)
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok().body(ratingModels);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
+    @GetMapping(value = "/id")
+    public ResponseEntity<RatingModel> getRatingById(@RequestParam long id) {
+        try {
+            Rating rating = ratingService.findRatingById(id);
+            return ResponseEntity.ok().body(converter.convertRatingModelToEntity(rating));
+        }
+    }
 
 }
