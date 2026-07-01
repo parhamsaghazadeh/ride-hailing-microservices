ab03c24a37d2d4d67962d1843368d5c77f47b0c5
Parham <parhamsaghazadeh@gmail.com>
2026-07-01 15:16:26 +0330

    add Payment

---
+++ services/payment-service/src/main/java/org/example/factory/entity/Payment.java
@@ -0,0 +1,31 @@
+package org.example.factory.entity;
+
+import jakarta.persistence.*;
+import org.example.factory.model.Enum;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payment")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "passenger_id",nullable = false)
+    private Long passengerId;
+    @Column(name = "driver_id",nullable = false)
+    private Long driverId;
+    @Column(name = "ride_id", nullable = false)
+    private Long rideId;
+    @Column(name = "amount",nullable = false)
+    private BigDecimal amount;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_status" , nullable = false)
+    private Enum.paymentStatus paymentStatus;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_status" , nullable = false)
+    private Enum.paymentMethod paymentMethod;
+    @Column(name = "payment_time" , nullable = false)
+    private LocalDateTime paymentTime;
+}
