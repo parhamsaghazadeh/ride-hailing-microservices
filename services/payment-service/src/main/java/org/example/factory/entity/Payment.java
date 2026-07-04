3e43f678cb820bf50988cf3bea2031677f66886c
Parham <parhamsaghazadeh@gmail.com>
2026-07-04 14:49:03 +0330

    fixe and change name field

--- services/payment-service/src/main/java/org/example/factory/entity/Payment.java
+++ services/payment-service/src/main/java/org/example/factory/entity/Payment.java
@@ -1,6 +1,7 @@
 package org.example.factory.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import org.example.factory.model.Enum;
 
 import java.math.BigDecimal;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "payment")
+@Data
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,8 +20,8 @@ public class Payment {
     private Long driverId;
     @Column(name = "ride_id", nullable = false)
     private Long rideId;
-    @Column(name = "amount",nullable = false)
-    private BigDecimal amount;
+    @Column(name = "ride_price",nullable = false)
+    private BigDecimal ridePrice;
     @Enumerated(EnumType.STRING)
     @Column(name = "payment_status" , nullable = false)
     private Enum.paymentStatus paymentStatus;
