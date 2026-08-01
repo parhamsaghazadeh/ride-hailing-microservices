fdd4c6d2f73da5a02d16b5e60b9081195ccb496f
Parham <parhamsaghazadeh@gmail.com>
2026-08-01 15:39:25 +0330

    fixe code

--- services/payment-service/src/main/java/org/example/factory/client/UserClient.java
+++ services/payment-service/src/main/java/org/example/factory/client/UserClient.java
@@ -1,15 +1,17 @@
 package org.example.factory.client;
 
 import org.example.factory.model.PersonWalletModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserClient {
+    @Autowired
     private RestTemplate restTemplate;
 
     public PersonWalletModel getPersonWalletById(Long personId){
         return restTemplate.getForObject(
-                "http://localhost:8080/user/Wallet/api" + personId, PersonWalletModel.class);
+                "http://localhost:8080/user/Wallet/" + personId, PersonWalletModel.class);
     }
 }
