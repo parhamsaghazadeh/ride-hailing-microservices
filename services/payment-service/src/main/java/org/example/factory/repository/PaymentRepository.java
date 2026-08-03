3e2f1e72a1a895edd9b0ae49feafaf84d3616da0
Parham <parhamsaghazadeh@gmail.com>
2026-08-03 13:35:06 +0330

    add PaymentRepository

--- services/payment-service/src/main/java/org/example/factory/repository/PaymentRepository.java
+++ services/payment-service/src/main/java/org/example/factory/repository/PaymentRepository.java
@@ -1,9 +1,10 @@
 package org.example.factory.repository;
 
 import org.example.factory.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaymentRepository extends CrudRepository<Payment, Long> {
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
