9a9c9114438cc88bb19df789960c2319c218da6f
Parham <parhamsaghazadeh@gmail.com>
2026-08-05 16:02:31 +0330

    add deposit and withdraw to personWallet

--- services/user-service/src/main/java/org/example/factory/service/PersonWalletService.java
+++ services/user-service/src/main/java/org/example/factory/service/PersonWalletService.java
@@ -1,5 +1,6 @@
 package org.example.factory.service;
 
+import jakarta.transaction.Transactional;
 import org.example.factory.repository.PersonWalletRepository;
 import org.example.factory.entity.PersonWallet;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -40,8 +43,44 @@ public class PersonWalletService {
 
     public PersonWallet findWalletById(Long id) {
         return personWalletRepository.findById(id).orElseGet(() -> {
-                       throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ride not found");
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ride not found");
                 }
         );
+    }
+
+
+    @Transactional
+    public PersonWallet deposit(Long id, BigDecimal amount) {
+        PersonWallet personWallet = findWalletById(id);
+
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Amount must be greater than 0");
+        }
+
+        personWallet.setBalance(personWallet.getBalance().add(amount));
+
+        personWallet.setUpdateAt(LocalDateTime.now());
+
+        return personWalletRepository.save(personWallet);
+    }
+
+
+    @Transactional
+    public PersonWallet withdraw(Long id, BigDecimal amount) {
+        PersonWallet personWallet = findWalletById(id);
+
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Amount must be greater than 0");
+        }
+
+        if (personWallet.getBalance().compareTo(amount) < 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Insufficient balance");
+        }
+
+        personWallet.setBalance(personWallet.getBalance().subtract(amount));
+
+        personWallet.setUpdateAt(LocalDateTime.now());
+
+        return personWalletRepository.save(personWallet);
     }
 }
